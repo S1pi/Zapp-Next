e585bf2356f66047f1f75757fbc50861ef87001e
hussainkazemian <hussain.kazemian@metropolia.fi>
2025-03-21 20:19:13 +0200

    live color dashboard are setted

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -22,43 +22,37 @@ export default function AdminLayout({
 
   return (
     <div className="min-h-screen flex flex-col bg-green-400">
-      {/* Header */}
-      <header className="bg-white shadow p-4">
-        <h1 className="text-2xl font-bold text-green-800">⚡ ZAPP Admin</h1>
-      </header>
-
+    
       {/* Main Content with Sidebar */}
       <div className="flex flex-1">
         {/* Sidebar */}
-        <aside className="w-64 bg-green-700 text-white p-4">
+        <aside className="w-64 bg-[#007f5f] text-white p-4">
           {/* Navigation */}
-          <nav className="mt-6">
+          <nav className="mt-60">
             <ul className="space-y-2">
               {adminMenu.map((item) => (
                 <li key={item.href}>
-                  <Link
-                    href={item.href}
-                    className={`flex items-center p-2 rounded-lg ${
-                      pathname === item.href
-                        ? "bg-green-600 text-white"
-                        : "text-gray-300 hover:bg-green-600 hover:text-white"
-                    }`}
-                  >
-                    <span className="mr-3">{item.icon}</span>
-                    {item.name}
-                  </Link>
+                <Link
+              href={item.href}
+              className={`flex items-center p-2 text-[#093331] hover:text-[#1AF3CF] hover:shadow-lg hover:shadow-[#1AF3CF] ${
+                pathname === item.href ? "text-[#093331]"  : ""
+              }`}
+            >
+              <span className="mr-3">{item.icon}</span>
+              {item.name}
+            </Link>
                 </li>
               ))}
             </ul>
           </nav>
 
           {/* User Profile / Logout */}
-          <div className="mt-auto p-4 border-t border-green-600">
+          <div className="m-auto mt-60 p-4 border-t border-green-600">
             <div className="flex items-center space-x-3  ">
               <div className="w-8 h-8 bg-gray-500 rounded-full "></div>
               <div>
-                <p className="text-sm">Username</p>
-                <button className="text-xs text-gray-300 hover:text-white">
+                <p className="text-sm text-white">Username</p>
+                <button className="text-xs text-[#093331] hover:text-[#1AF3CF] ">
                   Log out
                 </button>
               </div>
