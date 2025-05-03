44616c1c92af92975b75d8584d12d74fa80951e9
S1pi <miikavsipila@gmail.com>
2025-05-03 16:51:14 +0300

    feat: Update LiveDashboard to use server side session fetch so it can be rendered on server side

--- src/app/dashboard/live-dashboard/page.tsx
+++ src/app/dashboard/live-dashboard/page.tsx
@@ -1,15 +1,27 @@
-"use client";
+import { getUserSession } from "@/actions/authActions";
 import { CurrentStatus } from "@/components/live-dashboard/CurrentStatus";
 import { LastWeek } from "@/components/live-dashboard/LastWeek";
-import { useAdminSession } from "@/contexts/userContext";
 
-export default function LiveDashboard() {
-  const { userSession } = useAdminSession();
+export default async function LiveDashboard() {
+  const userSession = await getUserSession();
+
+  if (!userSession) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-h2 text-seabed-green mb-2 mt-5">
+          Ei käyttöoikeutta
+        </h1>
+        <p className="text-base text-seabed-green mt-4 py-4 border-t border-seperator-line">
+          Sinulla ei ole oikeuksia käyttää tätä sivua.
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h1 className="text-h2 text-seabed-green mb-2 mt-5">
-        Live Dashboard - {userSession?.dealership?.name}
+        Live Dashboard - {userSession.dealership?.name}
       </h1>
       <h2 className="text-base text-seabed-green mt-4 py-4 border-t border-seperator-line">
         Tällä hetkellä
