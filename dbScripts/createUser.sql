0baaf71b31124c7e9c66c1e15c3950c734849861
S1pi <miikavsipila@gmail.com>
2025-05-06 05:12:05 +0300

    Last changes

--- dbScripts/createUser.sql
+++ dbScripts/createUser.sql
@@ -1,3 +1,6 @@
 CREATE USER 'zapproot'@'localhost' IDENTIFIED BY 'password123!';
 GRANT ALL PRIVILEGES ON `zapp`.* TO 'zapproot'@'localhost';
 FLUSH PRIVILEGES;
+CREATE USER 'zapproot'@'localhost' IDENTIFIED BY 'ZapDealer69!';
+GRANT ALL PRIVILEGES ON `zapp`.* TO 'zapproot'@'localhost';
+FLUSH PRIVILEGES;
